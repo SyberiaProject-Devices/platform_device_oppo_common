b35c9dedab0dc9dfe9ef1ebfd468cf9cdfea8169
blinoff82 <blinov.in@gmail.com>
2019-07-09 11:36:57 +0300

    keyhandler: Remove duplicate jar class
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- common.mk
+++ common.mk
@@ -22,8 +22,6 @@ PRODUCT_PACKAGES += \
     ConfigPanel \
     com.cyanogenmod.keyhandler
 
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
-
 # never dexopt the keyhandler
 $(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 
